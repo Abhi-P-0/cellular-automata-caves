71b74d6891f473c267c28780ee611b18b11dbde3
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2024-05-16 19:55:15 -0400

    setup initial project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+static const int MAP_WIDTH = 100;
+static const int MAP_HEIGHT = 60;
+
+int ALIVE = 1;
+int DEAD = 0;
+
+int map[MAP_HEIGHT][MAP_WIDTH];
+
+int main() {
+    
+
+    return 0;
+}
+
