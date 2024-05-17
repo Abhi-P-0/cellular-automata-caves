cdd459f0dc2dc282c9d62c05b5a8047b5e30b551
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2024-05-16 20:00:20 -0400

    setup initial cave generation

--- main.cpp
+++ main.cpp
@@ -5,8 +5,33 @@ static const int MAP_HEIGHT = 60;
 
 int ALIVE = 1;
 int DEAD = 0;
+int seed = 1337;
+int aliveProb = 50;
 
 int map[MAP_HEIGHT][MAP_WIDTH];
+
+void genCave() {
+    srand(seed);
+
+    for (int h = 0; h < MAP_HEIGHT; h++) {
+        for (int w = 0; w < MAP_WIDTH; w++) {
+            map[h][w]=ALIVE;
+
+            if (rand() % 100 > aliveProb) {
+                map[h][w] = DEAD;
+
+            }
+
+        }
+
+    }
+
+}
+
+void displayCave() {
+    
+
+}
 
 int main() {
     
